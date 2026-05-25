c868bb5fd9ceb2c84f997e7eb06a0a0769c0e1f6
Sazkia07 <jonginaasazkia@gmail.com>
2026-05-26 00:33:20 +0700

    Update MenuFrame.java

--- MenuFrame.java
+++ MenuFrame.java
@@ -10,21 +10,24 @@ public class MenuFrame extends JFrame {
         JButton b = new JButton("Buku");
         JButton p = new JButton("Pegawai");
         JButton t = new JButton("Transaksi");
+        JButton k = new JButton("Pengembalian");
         JButton l = new JButton("Laporan");
 
         s.setBounds(100,20,200,30);
         b.setBounds(100,60,200,30);
         p.setBounds(100,100,200,30);
         t.setBounds(100,140,200,30);
-        l.setBounds(100,180,200,30);
+        k.setBounds(100,180,200,30);
+        l.setBounds (100,220,200,30);
 
-        add(s); add(b); add(p); add(t); add(l);
+        add(s); add(b); add(p); add(t); add(l); add(k);
 
         s.addActionListener(e -> new SiswaForm());
         b.addActionListener(e -> new BukuForm());
         p.addActionListener(e -> new PegawaiForm());
         t.addActionListener(e -> new TransaksiForm());
         l.addActionListener(e -> new LaporanForm());
+        k.addActionListener(e -> new KembaliForm());
 
         setVisible(true);
     }
